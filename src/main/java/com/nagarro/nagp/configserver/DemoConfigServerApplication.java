0a1f4915470f270768b88812523acc8a7d87a629
sahilsharma <sahil.sharma@nagarro.com>
2020-02-09 20:52:53 +0530

    add boilerplate code

---
+++ src/main/java/com/nagarro/nagp/configserver/DemoConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.nagp.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConfigServerApplication.class, args);
+	}
+
+}
